06b67d351b5626f27f7f4c0dbf15467fa061878c
HardhikErat <58391944+HardhikErat@users.noreply.github.com>
2020-03-23 20:38:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var video;
+
+function setup() {
+    createCanvas(640, 480);
+    background(51);
+    video = createCapture(VIDEO);
+    video.size(640, 480);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0);
+}
