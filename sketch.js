018997c4138f5b0bd1b057ea0a45d0aff6c96348
HardhikErat <58391944+HardhikErat@users.noreply.github.com>
2020-03-24 12:55:13 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var video;
 
 function setup() {
     createCanvas(640, 480);
-    background(51);
+    background("white");
     video = createCapture(VIDEO);
     video.size(640, 480);
     video.hide();
